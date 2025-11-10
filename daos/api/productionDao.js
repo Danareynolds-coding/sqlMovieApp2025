d4f027248086109eeb0c3d4aa04c24b634e6f019
Danareynolds-coding <dana.reynolds@mscoding.org>
2025-11-09 18:45:43 -0600

    finisged adding production

--- daos/api/productionDao.js
+++ daos/api/productionDao.js
@@ -3,35 +3,40 @@ const connect = require('../../config/dbconfig')
 
 const productionDao = {
   table:'production',
-  // findMovieByProduction:(res, table, id)=> {
-  //   let sql = `SELECT
-  //   p.production,
-  //   m.title AS movie_title 
-  //   FROM
-  //       production p
-  //   JOIN
-  //        movie m ON p.production_id = m.production_id;
+  findMovieByProduction:(res, table, id)=> {
+    let sql = `SELECT
+    p.production_id,
+    p.production,
+    m.movie_id,
+    m.title AS movie_title 
+    FROM
+        production AS p
+    JOIN
+         movie AS m ON p.production_id = m.production_id
+    WHERE
+         p.production_id = ?`
 
-  //   connect.query(
-  //     sql,
-  //     (error, rows) => {
-  //       if (!error) {
-  //         if (rows.length === 1) {
-  //           res.json(rows[0])
-  //         } else {
-  //           res.json(rows)
-  //         }
-  //       } else {
-  //         console.log('Dao error', error)
-  //         res.json({
-  //           "message": 'error',
-  //           'table': table,
-  //           'error': error
-  //         })
-  //       }
-  //     }
-  //   )
-  // }
+    connect.query(
+      sql,
+      [id],
+      (error, rows) => {
+        if (!error) {
+          if (rows.length === 1) {
+            res.json(rows[0])
+          } else {
+            res.json(rows)
+          }
+        } else {
+          console.log('Dao error', error)
+          res.json({
+            "message": 'error',
+            'table': table,
+            'error': error
+          })
+        }
+      }
+    )
+  }
 }
 
 module.exports = productionDao
